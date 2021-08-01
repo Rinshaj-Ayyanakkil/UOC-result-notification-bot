8a8098587b61603205548560102d77b00a0b1a4f
rinshaj <rinshajdx4@gmail.com>
2021-08-01 21:37:40 +0530

    Update config variables

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot_token = os.environ.get("BOT_TOKEN")
-random_url = "https://random-data-api.com/api/food/random_foo"
 channel_id = os.environ.get("RESULT_CHANNEL_ID")
 telegram_api_url=f"https://api.telegram.org/bot{bot_token}"
+exam_result_url="http://results.uoc.ac.in/getlist.php"
