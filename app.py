7d9deef26dcd14193d50d62c7b6e0e09877e6f6b
rinshaj <rinshajdx4@gmail.com>
2021-08-01 23:26:40 +0530

    Format the message with html

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ def main():
     while(True):
         print("checking...")
         new_results = search_results()
+        message:str = ""
         if new_results:
-            broadcast_message(new_results)
+            for result in new_results:
+                message += f"<b>{result}</b>\n"
+            message += """<a href='http://results.uoc.ac.in'>See Results</a>"""
+            broadcast_message(message)
         time.sleep(900)
 
 if __name__=="__main__":
