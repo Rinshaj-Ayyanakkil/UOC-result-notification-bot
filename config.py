0d31e35aab857f075a3aba9e35f2e644daa8abc1
rinshaj <rinshajdx4@gmail.com>
2021-08-14 10:18:08 +0530

    Add last result id as env variable

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 bot_token = os.environ.get("BOT_TOKEN")
 channel_id = os.environ.get("RESULT_CHANNEL_ID")
-telegram_api_url=f"https://api.telegram.org/bot{bot_token}"
-exam_result_url="http://results.uoc.ac.in/getlist.php"
-sleep_interval=os.environ.get("SLEEP_INTERVAL")
+telegram_api_url = f"https://api.telegram.org/bot{bot_token}"
+exam_result_url = "http://results.uoc.ac.in/getlist.php"
+sleep_interval = os.environ.get("SLEEP_INTERVAL")
+LAST_RESULT_ID = os.environ.get("LAST_RESULT_ID")
